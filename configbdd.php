1fa807d357e89f64b5615e60efe0f69ab24d9619
Fabien Pinel <fabien.pinel08@gmail.com>
2023-12-02 20:52:17 +0100

    ConfigBdd : Connexion à la base de données Always Data

--- configbdd.php
+++ configbdd.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $bdd = new PDO("mysql:host=localhost;dbname=squirrel;charset=utf8", "root", "");
+    $bdd = new PDO("mysql:host=mysql-squirel.alwaysdata.net;dbname=squirel_bdd;charset=utf8mb4", "squirel", "Squirel13013@");
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
