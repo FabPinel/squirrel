76e3402dd8e69b0b57562e93c639d3579d80023e
Fabien Pinel <fabien.pinel08@gmail.com>
2023-12-09 11:57:57 +0100

    Login : Bloquer la connexion des utilisateurs ban

--- ressources/class/UserBanned.php
+++ ressources/class/UserBanned.php
@@ -1,14 +1,14 @@
 <?php
-
+require_once('../Class/User.php');
 class UserBanned extends User
 {
     private int $id;
     private User $user;
     private string $message;
-    private DateTime $createdDate;
+    private string $createdDate;
     private User $bannedBy;
 
-    public function __construct(int $id, User $user, string $message, DateTime $createdDate, User $bannedBy)
+    public function __construct(int $id, User $user, string $message, string $createdDate, User $bannedBy)
     {
         $this->id = $id;
         $this->user = $user;
@@ -47,12 +47,12 @@ class UserBanned extends User
         $this->message = $message;
     }
 
-    public function getCreatedDateBan(): DateTime
+    public function getCreatedDateBan(): string
     {
         return $this->createdDate;
     }
 
-    public function setCreatedDateBan(DateTime $createdDate): void
+    public function setCreatedDateBan(string $createdDate): void
     {
         $this->createdDate = $createdDate;
     }
@@ -65,5 +65,82 @@ class UserBanned extends User
     public function setBannedBy(User $bannedBy): void
     {
         $this->bannedBy = $bannedBy;
+    }
+
+    public static function getAllUsersBanned($bdd)
+    {
+        $queryUsersBanned = $bdd->prepare("SELECT b.*,
+            u.nickname as user_nickname, 
+            u.id as user_id, 
+            u.lastName as user_lastName, 
+            u.firstName as user_firstName, 
+            u.email as user_email, 
+            u.role as user_role, 
+            u.banner as user_banner, 
+            u.picture as user_picture, 
+            u.bio as user_bio, 
+            u.createdDate as user_createdDate, 
+            u.birthday as user_birthday, 
+            u.isVerify as user_isVerify,
+            bannedByUser.nickname as bannedBy_nickname,
+            bannedByUser.id as bannedBy_id,
+            bannedByUser.lastName as bannedBy_lastName,
+            bannedByUser.firstName as bannedBy_firstName,
+            bannedByUser.email as bannedBy_email,
+            bannedByUser.role as bannedBy_role,
+            bannedByUser.banner as bannedBy_banner,
+            bannedByUser.picture as bannedBy_picture,
+            bannedByUser.bio as bannedBy_bio,
+            bannedByUser.createdDate as bannedBy_createdDate,
+            bannedByUser.birthday as bannedBy_birthday,
+            bannedByUser.isVerify as bannedBy_isVerify
+            FROM bans b
+            JOIN users u ON b.user = u.id
+            JOIN users bannedByUser ON b.bannedBy = bannedByUser.id");
+        $queryUsersBanned->execute();
+
+        $banned = [];
+
+        while ($row = $queryUsersBanned->fetch(PDO::FETCH_ASSOC)) {
+            $user = new User(
+                $row['user_id'],
+                $row['user_firstName'],
+                $row['user_lastName'],
+                $row['user_nickname'],
+                $row['user_email'],
+                $row['user_picture'],
+                $row['user_banner'],
+                $row['user_bio'],
+                $row['user_role'],
+                $row['user_createdDate'],
+                $row['user_birthday'],
+                $row['user_isVerify']
+            );
+
+            $bannedByUser = new User(
+                $row['bannedBy_id'],
+                $row['bannedBy_firstName'],
+                $row['bannedBy_lastName'],
+                $row['bannedBy_nickname'],
+                $row['bannedBy_email'],
+                $row['bannedBy_picture'],
+                $row['bannedBy_banner'],
+                $row['bannedBy_bio'],
+                $row['bannedBy_role'],
+                $row['bannedBy_createdDate'],
+                $row['bannedBy_birthday'],
+                $row['bannedBy_isVerify']
+            );
+
+            $banned[] = new UserBanned(
+                $row['id'],
+                $user,
+                $row['msg'],
+                $row['createdDate'],
+                $bannedByUser
+            );
+        }
+
+        return $banned;
     }
 }
--- ressources/controller/loginController.php
+++ ressources/controller/loginController.php
@@ -3,6 +3,9 @@
 session_start(); // Démarre la session
 //connection à la bdd
 require_once('../../configbdd.php');
+require_once('../Class/UserBanned.php'); ?>
+<link rel="stylesheet" href="../css/login.css">
+<?php
 
 if (!empty($_POST['email']) && !empty($_POST['password'])) {
     $email = htmlspecialchars($_POST['email']);
@@ -19,6 +22,22 @@ if (!empty($_POST['email']) && !empty($_POST['password'])) {
     if ($row > 0) {
         if (filter_var($email, FILTER_VALIDATE_EMAIL)) {  //on vérifie que l'email ait la bonne forme
             if (password_verify($password, $verif_user['password'])) {
+                // Vérification si l'utilisateur est banni
+                $bannedUsers = UserBanned::getAllUsersBanned($bdd);
+
+                foreach ($bannedUsers as $bannedUser) {
+                    if ($bannedUser->getUser()->getId() == $verif_user['id']) { ?>
+                        <div class="center">
+                            <h2>Le marteau du bannissement a frappé</h2>
+                            <div class="banni">Impossible de vous connecter, vous êtes banni</div>
+                            <a href="/index.php">
+                                <button class="BtnRetour"> Retourner à la page d'accueil</button>
+                            </a>
+                        </div>
+<?php
+                        die();
+                    }
+                }
                 $_SESSION['user'] = $verif_user['id'];
                 header('Location: ' . $_SESSION['current_page']);
                 die();
