6a9d75a5590e863f79c255efccdefbcda5e24397
Fabien Pinel <fabien.pinel08@gmail.com>
2023-12-09 16:27:31 +0100

    Ancre: Affichage à partir du scroll

--- ressources/views/ancre.php
+++ ressources/views/ancre.php
@@ -1,10 +1,22 @@
 <script>
+    window.addEventListener('scroll', function() {
+        var scrollPosition = window.scrollY;
+
+        // Ajouter la classe "visible" lorsque la position de défilement dépasse 200 pixels
+        if (scrollPosition > 200) {
+            document.querySelector('.ancre').classList.add('visible');
+        } else {
+            // Retirer la classe "visible" si la position de défilement est inférieure à 200 pixels
+            document.querySelector('.ancre').classList.remove('visible');
+        }
+    });
+
     function goTop() {
         window.scrollTo({
             top: 0,
             left: 0,
-            behavior: "smooth"
-        })
+            behavior: 'smooth'
+        });
     }
 </script>
 
