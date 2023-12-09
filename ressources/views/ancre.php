3c9fdefc5d9b8ad96fec98ffd4ae8284370fa682
Fabien Pinel <fabien.pinel08@gmail.com>
2023-12-09 15:15:06 +0100

    Ajout d'une ancre pour remonter en haut de page

---
+++ ressources/views/ancre.php
@@ -0,0 +1,15 @@
+<script>
+    function goTop() {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        })
+    }
+</script>
+
+<div class="ancre">
+    <span class="material-symbols-outlined arrowTop" onclick="goTop()">
+        arrow_upward
+    </span>
+</div>
